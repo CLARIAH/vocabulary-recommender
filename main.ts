8fcb9e1f6daf1e02b299d4fec8f17e31da2c3653
Jana Grimm <jana.grimm@triply.cc>
2022-09-08 17:00:45 +0200

    FIXED recommendation function for elastic search endpoints

--- main.ts
+++ main.ts
@@ -31,9 +31,9 @@ select distinct ?iri ?description {
       rdfs:label ?label.
   }
 }
-limit 1`
+limit 10`
 
-const PREDICATE_SEARCH_CONFIG = (term: string) => `prefix owl: <http://www.w3.org/2002/07/owl#>
+const PREDICATE_SEARCH_QUERY = (term: string) => `prefix owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 select distinct ?iri ?description {
@@ -70,7 +70,26 @@ select distinct ?iri ?description {
       rdfs:label ?label.
   }
 }
-limit 1`
+limit 10`
+
+const CLASS_SEARCH_CONFIG = [
+  {
+    simple_query_string: {
+      query: '"http://www.w3.org/2002/07/owl#Class" | "http://www.w3.org/2000/01/rdf-schema#Class"',
+      fields: ["http://www w3 org/1999/02/22-rdf-syntax-ns#type"],
+    },
+  },
+];
+
+const PREDICATE_SEARCH_CONFIG = [
+  {
+    simple_query_string: {
+      query:
+        '"http://www.w3.org/2002/07/owl#DatatypeProperty" | "http://www.w3.org/2002/07/owl#ObjectProperty" | "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"',
+      fields: ["http://www w3 org/1999/02/22-rdf-syntax-ns#type"],
+    },
+  },
+];
 
 enum Category {
   class = 'class',
@@ -105,7 +124,7 @@ function assignQuery(category: Category) {
   if(category === Category.class) {
     return  CLASS_SEARCH_QUERY
   } else if (category === Category.property) {
-    return PREDICATE_SEARCH_CONFIG
+    return PREDICATE_SEARCH_QUERY
   } else {
     throw Error('Category does not exist! Please provide existing category.')
   }
@@ -147,43 +166,23 @@ function getSuggestionFromBody(responseBody: ShardResponse): AutocompleteSuggest
 }
 
 async function elasticSuggestions(category: Category, term: string, endpoint: string) {
-  const searchObject = {
+  const searchObject =  { 
     query: {
-      bool: {
-        must: category,
-        should: [
-          // Search for wildcard
-          {
-            wildcard: { "http://www w3 org/2000/01/rdf-schema#label": term + "*" },
-          },
-          // Catch spelling mistakes
-          { fuzzy: { "http://www w3 org/2000/01/rdf-schema#label": term } },
-          // Match full IRI
-          {
-            match: { "@id": term },
-          },
-          // Search for words in descriptions
-          {
-            simple_query_string: {
-              query: term,
-              fields: ["http://www w3 org/2000/01/rdf-schema#comment"],
-            }
-          }
-        ],
-        minimum_should_match: 1
+      simple_query_string: {
+        query: term
       }
     }
   }
+  
   const fetch = require('node-fetch')
   const response = await fetch(
     endpoint, 
-    // {
-    //   method: "POST",
-    //   body: JSON.stringify(searchObject),
-    // }
+    {
+      method: "POST",
+      body: JSON.stringify(searchObject),
+    }
   )
   const json: ShardResponse = await response.json()
-  console.log(response)
   return getSuggestionFromBody(json)
 }
 
@@ -194,15 +193,16 @@ async function run() {
   const searchTerm = 'person'
   const sparqlEndpoint = 'https://api.data.netwerkdigitaalerfgoed.nl/datasets/ld-wizard/sdo/services/sparql/sparql'
   const elasticEndpoint = 'https://api.triplydb.com/datasets/smithsonian/american-art-museum/services/american-art-museum-1/elasticsearch'
-
-  const sparqlSuggested = sparqlSuggestions(category, searchTerm, sparqlEndpoint)
-  const elasticSuggested = elasticSuggestions(category, searchTerm, elasticEndpoint)
+  //const elasticEndpoint = 'https://api.druid.datalegend.net/datasets/VocabularyRecommender/RecommendedVocabularies/services/RecommendedVocabularies/search'
+  
+  const sparqlSuggested = await sparqlSuggestions(category, searchTerm, sparqlEndpoint)
+  const elasticSuggested = await elasticSuggestions(category, searchTerm, elasticEndpoint)
 
   console.log(`This is what you were looking for:\ncategory: ${category},\nsearchTerm: ${searchTerm},\nendpoint: ${sparqlEndpoint}\n\n`)
-  console.log(`\n\n`)
-
-  //console.log(await sparqlSuggested)
-  console.log(await elasticSuggested)
+  console.log(`\n\nSparql suggestions:\n`)
+  console.log(sparqlSuggested)
+  console.log(`\n\nElasticsearch suggestions:\n`)
+  console.log(elasticSuggested)
 }
 run().catch(e => {
   console.error(e)
