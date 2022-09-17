2f697d99d641f843af4b2f8eff7ec168f2014a07
Kathrin Dentler <kathrin@dentler.org>
2022-09-17 10:34:04 +0200

    REMOVE: unused prefix

--- src/sparql.ts
+++ src/sparql.ts
@@ -8,7 +8,6 @@ export interface SparqlResult {
 export const CLASS_SEARCH_SPARQL_QUERY = (
   term: string
 ) => `prefix owl: <http://www.w3.org/2002/07/owl#>
-  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   select distinct ?iri ?description {
     {
